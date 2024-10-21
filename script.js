0b87bacfe7ed3729899f0bcf2bb795e0da6ab9a6
Magestick <74025726+Magest1ckkz@users.noreply.github.com>
2024-10-21 17:45:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+$(document).ready(function() {
+    $("#draggable").draggable();
+  });
+  $(document).ready(function() {
+    $("#draggable2").draggable();
+  });
